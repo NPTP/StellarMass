523f700b0535419b08133f73a65582ad9a323664
Nick Perrin <nick.perrin@hotmail.com>
2024-04-21 19:39:19 -0400

    Frame delay coroutine helper

---
+++ Assets/Scripts/Utilities/CoroutineAid.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using Utilities;
+
+namespace StellarMass.Utilities
+{
+    public static class CoroutineAid
+    {
+        public static void Delay1Frame(Action callback) => DelayByFrames(1, callback);
+        public static void DelayByFrames(int frames, Action callback)
+        {
+            frames = Mathf.Max(0, frames);
+            CoroutineOwner.StartRoutine(delayRoutine());
+            
+            IEnumerator delayRoutine()
+            {
+                for (int f = 0; f < frames; f++)
+                {
+                    yield return null;
+                }
+                
+                callback?.Invoke();
+            }
+        }
+    }
+}
