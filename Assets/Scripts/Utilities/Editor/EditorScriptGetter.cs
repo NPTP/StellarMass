5ff3a167d949483e64b1b099369bbbcb5f5606bb
Nick Perrin <nick.perrin@hotmail.com>
2024-05-03 14:37:16 -0400

    EditorScriptGetter support enum types

--- Assets/Scripts/Utilities/Editor/EditorScriptGetter.cs
+++ Assets/Scripts/Utilities/Editor/EditorScriptGetter.cs
@@ -10,12 +10,21 @@ namespace StellarMass.Utilities.Editor
 
         public static string GetSystemPath(Type type)
         {
+            bool isEnum = type.IsEnum;
+            
             string[] guids = AssetDatabase.FindAssets($"t:Script");
             foreach (string guid in guids)
             {
                 string assetPath = AssetDatabase.GUIDToAssetPath(guid);
                 MonoScript scriptAsset = AssetDatabase.LoadAssetAtPath<MonoScript>(assetPath);
-                if (scriptAsset != null && type.IsAssignableFrom(scriptAsset.GetClass()))
+
+                if (scriptAsset == null)
+                {
+                    continue;
+                }
+
+                if ((isEnum && scriptAsset.text.Contains($"enum {type.Name}")) ||
+                    scriptAsset.GetClass() == type || type.IsAssignableFrom(scriptAsset.GetClass()))
                 {
                     return Application.dataPath + assetPath.Replace("Assets", string.Empty);
                 }
