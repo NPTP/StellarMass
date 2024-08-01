b1b372b1fa3d888622a2f79c9fd4e97d6763c505
Nick Perrin <nick.perrin@hotmail.com>
2024-07-31 22:29:46 -0400

    Update AddressablesUtility with useful methods

--- Assets/Scripts/Utilities/AddressablesUtility.cs
+++ Assets/Scripts/Utilities/AddressablesUtility.cs
@@ -1,11 +1,13 @@
-using UnityEngine;
+using System;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using Object = UnityEngine.Object;
 
 #if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.AddressableAssets;
 using UnityEditor.AddressableAssets.Settings;
+using UnityEditor.AddressableAssets.Settings.GroupSchemas;
 #endif
 
 namespace StellarMass.Utilities
@@ -14,51 +16,91 @@ namespace StellarMass.Utilities
     {
         public static T LoadAssetSynchronous<T>(string key)
         {
-            T asset;
-            AsyncOperationHandle<T> handle = Addressables.LoadAssetAsync<T>(key);
 #if UNITY_WEBGL
             // WebGL is single-threaded, and calling WaitForCompletion can lock it, so we use the normal async workflow for this case.
             // See https://docs.unity3d.com/Packages/com.unity.addressables@1.20/manual/SynchronousAddressables.html
-            handle.Completed += onCompleted;
-            void onCompleted(AsyncOperationHandle<T> asyncOperationHandle)
-            {
-                if (asyncOperationHandle.Status is AsyncOperationStatus.Succeeded)
-                    asset = asyncOperationHandle.Result;
-            }
-#else
+            return default;
+#endif
+            T asset;
+            AsyncOperationHandle<T> handle = Addressables.LoadAssetAsync<T>(key);
             asset = handle.WaitForCompletion();
             Addressables.Release(handle);
-#endif
             return asset;
         }
-        
-#if UNITY_EDITOR
-        public static void EDITOR_SetAddress(this Object unityObject, string address, bool createIfNotAddressable = false)
+
+        public static void LoadAssetAsync<T>(string key, Action<T> completedHandler)
         {
-            AddressableAssetEntry entry = createIfNotAddressable ? EDITOR_AddObjectToAddressablesDefaultSettings(unityObject) : EDITOR_GetEntry(unityObject);
-            entry.address = address;
+            AsyncOperationHandle<T> handle = Addressables.LoadAssetAsync<T>(key);
+            handle.Completed += operationHandle =>
+            {
+                if (operationHandle.Status != AsyncOperationStatus.Succeeded)
+                {
+                    return;
+                }
+
+                completedHandler?.Invoke(operationHandle.Result);
+                Addressables.Release(handle);
+            };
+        }
+
+#if UNITY_EDITOR
+        private static string EDITOR_ToGuid(Object unityObject) => AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(unityObject));
+
+        public static AddressableAssetEntry EDITOR_AddObjectToAddressablesGroup(Object unityObject, string groupName)
+        {
+            return EDITOR_AddObjectToAddressablesGroupInternal(unityObject, useDefaultGroup: false, groupName);
         }
         
-        public static AddressableAssetEntry EDITOR_AddObjectToAddressablesDefaultSettings(Object unityObject)
+        public static AddressableAssetEntry EDITOR_AddObjectToAddressablesDefaultGroup(Object unityObject)
         {
-            AddressableAssetEntry assetEntry = EDITOR_GetEntry(unityObject);
-            if (assetEntry == null)
+            return EDITOR_AddObjectToAddressablesGroupInternal(unityObject, useDefaultGroup: true);
+        }
+
+        private static AddressableAssetEntry EDITOR_AddObjectToAddressablesGroupInternal(Object unityObject, bool useDefaultGroup, string groupName = "")
+        {
+            if (EDITOR_TryGetEntry(unityObject, out AddressableAssetEntry entry))
             {
-                assetEntry = AddressableAssetSettingsDefaultObject.Settings.CreateOrMoveEntry(
-                    AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(unityObject)),
-                    AddressableAssetSettingsDefaultObject.Settings.DefaultGroup);
-                EditorUtility.SetDirty(AddressableAssetSettingsDefaultObject.Settings);
+                return entry;
+            }
+            
+            AddressableAssetSettings settings = AddressableAssetSettingsDefaultObject.Settings;
+            AddressableAssetGroup group = settings.DefaultGroup;
+                
+            if (!useDefaultGroup && !string.IsNullOrEmpty(groupName))
+            {
+                group = settings.FindGroup(groupName);
+                if (group == null)
+                {
+                    group = settings.CreateGroup(groupName, true, false, false, null,
+                        typeof(ContentUpdateGroupSchema), typeof(BundledAssetGroupSchema));
+                }
             }
 
-            return assetEntry;
+            entry = settings.CreateOrMoveEntry(EDITOR_ToGuid(unityObject), group, false, false);
+
+            group.SetDirty(AddressableAssetSettings.ModificationEvent.EntryMoved, entry, false, true);
+            settings.SetDirty(AddressableAssetSettings.ModificationEvent.EntryMoved, entry, true, false);
+            
+            return entry;
         }
 
-        public static AddressableAssetEntry EDITOR_GetEntry(Object unityObject)
+        public static void EDITOR_RemoveEntry(Object unityObject)
         {
-            string path = AssetDatabase.GetAssetPath(unityObject);
-            string guid = AssetDatabase.AssetPathToGUID(path);
+            AddressableAssetSettingsDefaultObject.Settings.RemoveAssetEntry(EDITOR_ToGuid(unityObject));
+        }
 
-            return AddressableAssetSettingsDefaultObject.Settings.FindAssetEntry(guid);
+        public static bool EDITOR_TryGetEntry(Object unityObject, out AddressableAssetEntry entry)
+        {
+            entry = AddressableAssetSettingsDefaultObject.Settings.FindAssetEntry(EDITOR_ToGuid(unityObject));
+            return entry != null;
+        }
+
+        public static void EDITOR_SetAddress(this Object unityObject, string address, bool createIfNotAddressable = false)
+        {
+            if (EDITOR_TryGetEntry(unityObject, out AddressableAssetEntry entry))
+            {
+                entry.address = address;
+            }
         }
 #endif
     }
