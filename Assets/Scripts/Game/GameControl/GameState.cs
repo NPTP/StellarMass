fb29b6e6f6edbfec526a1590ae62d3364aee5389
Nick Perrin <nick.perrin@hotmail.com>
2024-12-11 10:47:51 -0500

    Clean up GameState

--- Assets/Scripts/Game/GameControl/GameState.cs
+++ Assets/Scripts/Game/GameControl/GameState.cs
@@ -5,17 +5,17 @@ namespace Summoner.Game.GameControl
     public static class GameState
     {
         [Flags]
-        public enum GameStateFlags
+        private enum GameStateFlags
         {
             Gameplay = 1 << 1,
             Cutscene = 1 << 2,
             Pause = 1 << 3
         }
 
-        private static GameStateFlags stateFlags;
-
         public static event Action OnGameStateChanged;
         
+        private static GameStateFlags stateFlags;
+
         public static bool InGameplay
         {
             get => HasFlag(GameStateFlags.Gameplay);
@@ -37,31 +37,21 @@ namespace Summoner.Game.GameControl
         public static bool CanPause => InGameplay && !InPause;
         public static bool CanUnpause => InPause;
 
-        private static bool HasFlag(GameStateFlags flag) => stateFlags.HasFlag(flag);
+        private static bool HasFlag(GameStateFlags flag) => (stateFlags & flag) != 0;
 
         private static void SetFlag(GameStateFlags flag, bool value)
         {
             GameStateFlags previous = stateFlags;
-            if (value) stateFlags |= flag;
-            else stateFlags &= ~flag;
+            if (value) stateFlags.Add(flag);
+            else stateFlags.Remove(flag);
             
             if (stateFlags != previous)
             {
                 OnGameStateChanged?.Invoke();
             }
         }
-    }
-
-    public static class FlagsExtensions
-    {
-        public static void AddFlag(this GameState.GameStateFlags flags, GameState.GameStateFlags add)
-        {
-            flags |= add;
-        }
-
-        public static void RemoveFlag(this GameState.GameStateFlags flags, GameState.GameStateFlags remove)
-        {
-            flags &= ~remove;
-        }
+        
+        private static void Add(this ref GameStateFlags flags, GameStateFlags add) => flags |= add;
+        private static void Remove(this ref GameStateFlags flags, GameStateFlags remove) => flags &= ~remove;
     }
 }
